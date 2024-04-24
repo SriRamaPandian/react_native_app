bb8e175cb85a68b41ff1aa5ec851688495173f31
SriRamaPandian <sriramapandian147@gmail.com>
2024-04-24 06:47:21 +0530

    login

--- app/SelectCourse.js
+++ app/SelectCourse.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Text , View , Button } from 'react-native';
+import { useRoute } from '@react-navigation/native';
+
 
 const SelectCourse = ({ navigation }) => {
+  const route = useRoute();
+  const {rollno} = route.params || {};
+
       return (
         <View>
-        <Text>Select Your Course</Text>
+        <Text>Select Your Course and your rollno is {rollno}</Text>
           <Button
             title="next"
-            onPress={() => navigation.navigate('Final')} />
+            onPress={() => navigation.navigate('Final',)} />
         </View>
       );
     }
